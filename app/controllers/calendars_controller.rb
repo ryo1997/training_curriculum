9b2f9ca4090627539038537557d0b72497766dff
ryo1997 <youarethexbox360@gmail.com>
2020-09-27 20:01:57 +0900

    issue6　修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -35,12 +35,13 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
       wday_num = x
-      if wday_num => 7
+      if wday_num > 7
         wday_num = wday_num - 7
       end
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdaysから値を取り出す記述}
+      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wday: wdays[(@todays_date + x).wday] }
       @week_days.push(days)
     end
-
   end
 end
+
+
